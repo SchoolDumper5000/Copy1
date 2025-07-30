3d199c93fa596221cba6f505ddf7ab418ad4d106
Ali Banks <40315505-alibanksproject@users.noreply.replit.com>
2025-07-30 17:20:14 +0000

    Restored to 'e999d66cea8259af96ca17d9ca29a8413ea934ef'
    Replit-Restored-To: e999d66cea8259af96ca17d9ca29a8413ea934ef

--- test.py
+++ test.py
@@ -1,17 +1,6 @@
-
-import survey
-
-# Simple test
-options = ["Option 1", "Option 2", "Option 3"]
-print("About to show survey menu...")
-try:
-    index = survey.routines.select(
-        'Please select an option:\n',
-        options=options,
-        focus_mark='> ',
-        evade_color=survey.colors.basic('white'),
-        insearch_color=survey.colors.basic('white')
-    )
-    print(f"You selected: {options[index]} (index: {index})")
-except Exception as e:
-    print(f"Error: {e}")
+index = survey.routines.select(
+  'Ｐｌｅａｓｅ ｓｅｌｅｃｔ ｔｈｅ ｏｐｔｉｏｎｓ ｔｏ ｆｉｌｌ ｉｎ ｔｈｅ ｃｒｅｄｅｎｔｉａｌｓ:\n',
+  options=options,
+  focus_mark='> ',
+  evade_color=survey.colors.basic('white'),
+  insearch_color=survey.colors.basic('white'))
